503946f6ee3bbad3dde30ab9fdd46cdb7a570ecf
dementrov4 <alinasoloveva105@gmail.com>
2022-04-10 21:35:02 +0500

    Create Init Structure Project. Add Lexems

---
+++ inherence_check/inherence_lib/lexems.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+LOGICAL_OPERATORS: List[str] = [
+    'and',
+    'not',
+    'or',
+]
+
+LOGICAL_RELATIONS: List[str] = [
+    '|-',
+    '->',
+    '<-',
+]
+
+BRACKETS: List[str] = [
+    '(',
+    ')',
+    '[',
+    ']',
+    '|',
+]
+
+KEY_WORDS: List[str] = [
+    'forall',
+    'exist',
+]
+
+SPECIAL_SYMBOLS: List[str] = [
+    '_',
+    '^',
+    '{',
+    '}',
+    '\\',
+]
+
+GROUP_FORMULAS: List[str] = [
+    'G',
+]
